e9f378c36085aadfa8da7eb8d817f93579f77e31
TyuuutoT <takara-yuuto-2205@occ.co.jp>
2022-05-31 09:51:43 +0900

    変更

--- Models/MS_ORGANIZATION.cs
+++ Models/MS_ORGANIZATION.cs
@@ -1,18 +1,19 @@
-﻿namespace ThanksCardAPI.Models
+﻿#nullable disable
+namespace ThanksCardAPI.Models
 {
     public class MS_ORGANIZATION
     {
         public long Id { get; set; }
-        public int ORGANIZATION_CD { get; set; }
-        public string? ORGANIZATION_NAME { get; set; }
+        public int Organiztioncd { get; set; }
+        public string Organiztionname { get; set; }
 
         // 部と課のような階層構造を実現するために、自己参照(DepartmentエンティティがDepartmentエンティティとリレーションを持つ)を利用する
         // ParentId には親部署のIdが入る
         // Parent には親部署が入る
         // Children には子部署リストが入る
-        public long? PARENT_ID { get; set; }
-        public virtual Department Parent { get; set; }
-        public virtual ICollection<Department> Children { get; set; }
+        public long? ParentId { get; set; }
+        public virtual MS_ORGANIZATION Parent { get; set; }
+        public virtual ICollection<MS_ORGANIZATION> Children { get; set; }
 
         // 1対多: Department エンティティには複数の User エンティティが属する
         public virtual ICollection<MS_EMPLOYEE> EMPLOYEEs { get; set; }
